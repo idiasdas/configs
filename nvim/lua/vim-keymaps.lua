3f84deedbcc90cf67be6ef88f44a6d09e7a549a3
idiasdas <igordias.ti@gmail.com>
2024-10-16 18:35:20 +0200

    [FEAT] Add keymaps no move selected text

--- nvim/lua/vim-keymaps.lua
+++ nvim/lua/vim-keymaps.lua
@@ -54,3 +54,7 @@ vim.keymap.set("i", "<A-]>", "[]")
 vim.keymap.set("i", "<A-{>", "{}<Left>")
 vim.keymap.set("i", "<A-}>", "{}")
 
+-- Move selected text
+
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
