ec0c598ff4413ce332dc60aa3d745ee5293599d2
idiasdas <igordias.ti@gmail.com>
2024-12-11 19:51:41 +0100

    Add fzf to telescope. Treesitter autoinstall = false. lsp show_line = false

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "cmake", "pylsp" },
+                ensure_installed = { "lua_ls", "clangd", "cmake", "pylsp", "bashls" },
             })
         end,
     },
@@ -18,17 +18,22 @@ return {
         config = function()
             local capabilities = require("cmp_nvim_lsp").default_capabilities()
             local lspconfig = require("lspconfig")
+            local telescope = require("telescope.builtin")
 
             local on_attach = function(_, bufnr)
                 local map = function(keys, func, desc, mode)
                     mode = mode or "n"
                     vim.keymap.set(mode, keys, func, { buffer = bufnr, desc = "LSP: " .. desc })
                 end
-                map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
-                map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
-                map("<leader>gt", require("telescope.builtin").lsp_type_definitions, "[G]o to [T]ype Definition")
-                map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
-                map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+                map("gd", function()
+                    telescope.lsp_definitions({ show_line = false })
+                end, "[G]oto [D]efinition")
+                map("gr", function()
+                    telescope.lsp_references({ show_line = false })
+                end, "[G]oto [R]eferences")
+                map("<leader>gt", telescope.lsp_type_definitions, "[G]o to [T]ype Definition")
+                map("<leader>ds", telescope.lsp_document_symbols, "[D]ocument [S]ymbols")
+                map("<leader>ws", telescope.lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
                 map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
                 map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction", { "n", "x" })
                 map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
@@ -52,6 +57,10 @@ return {
                 on_attach = on_attach,
             })
             lspconfig.pylsp.setup({
+                capabilities = capabilities,
+                on_attach = on_attach,
+            })
+            lspconfig.bashls.setup({
                 capabilities = capabilities,
                 on_attach = on_attach,
             })
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,56 +1,61 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.8",
-		dependencies = { "nvim-lua/plenary.nvim" },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", function()
-				builtin.find_files({ hidden = true })
-			end, { desc = "Telescope: Find files" })
-			vim.keymap.set("n", "<leader>si", function()
-				builtin.find_files({ hidden = true, no_ignore = true })
-			end, { desc = "Telescope: Find files (include .gitignore)" })
-			vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope: [S]earch [G]rep" })
-			vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope: [S]earch [H]elp" })
-			vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Telescope: [S]earch [K]eymaps" })
-			vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Telescope: [S]earch [D]iagnostics" })
-			vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Telescope: [S]earch [R]esume" })
-			vim.keymap.set(
-				"n",
-				"<leader>s.",
-				builtin.oldfiles,
-				{ desc = "Telescope: [S]earch Recent Files ('.' for repeat)" }
-			)
-			vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope: Find existing buffers" })
-			vim.keymap.set("n", "<leader>sn", function()
-				builtin.find_files({ cwd = vim.fn.stdpath("config") })
-			end, { desc = "Telescope: [S]earch [N]eovim files" })
-			vim.keymap.set("n", "<leader>sN", function()
-				builtin.find_files({ cwd = "$HOME/Documents/notes" })
-			end, { desc = "Telescope: [S]earch [N]eovim files" })
-			vim.keymap.set("n", "<C-f>", builtin.grep_string, { desc = "Telescope: Grep word" })
-			vim.keymap.set("n", "<leader>tp", function()
-				builtin.planets({ show_pluto = true })
-			end, { desc = "Telescope: Grep word" })
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				defaults = {
-					layout_strategy = "vertical",
-					initial_value = "normal",
-					path_display = { "absolute" },
-				},
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.8",
+        dependencies = { "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope-ui-select.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
+        config = function()
+            -- setup
+            require("telescope").setup({
+                defaults = {
+                    layout_strategy = "vertical",
+                    initial_value = "normal",
+                    path_display = { "absolute" },
+                },
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                    fzf = {},
+                },
+            })
+
+            -- extensions
+            require("telescope").load_extension("fzf")
+            require("telescope").load_extension("ui-select")
+
+            -- keymaps
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<C-p>", function()
+                builtin.find_files({ hidden = true })
+            end, { desc = "Telescope: Find files" })
+            vim.keymap.set("n", "<leader>si", function()
+                builtin.find_files({ hidden = true, no_ignore = true })
+            end, { desc = "Telescope: Find files (include .gitignore)" })
+            vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Telescope: [S]earch [G]rep" })
+            vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Telescope: [S]earch [H]elp" })
+            vim.keymap.set("n", "<leader>sk", builtin.keymaps, { desc = "Telescope: [S]earch [K]eymaps" })
+            vim.keymap.set("n", "<leader>sd", builtin.diagnostics, { desc = "Telescope: [S]earch [D]iagnostics" })
+            vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "Telescope: [S]earch [R]esume" })
+            vim.keymap.set(
+                "n",
+                "<leader>s.",
+                builtin.oldfiles,
+                { desc = "Telescope: [S]earch Recent Files ('.' for repeat)" }
+            )
+            vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Telescope: Find existing buffers" })
+            vim.keymap.set("n", "<leader>sn", function()
+                builtin.find_files({ cwd = vim.fn.stdpath("config") })
+            end, { desc = "Telescope: [S]earch [N]eovim files" })
+            vim.keymap.set("n", "<leader>sN", function()
+                builtin.find_files({ cwd = "$HOME/Documents/notes" })
+            end, { desc = "Telescope: [S]earch [N]otes" })
+            vim.keymap.set("n", "<C-f>", builtin.grep_string, { desc = "Telescope: Grep word" })
+            vim.keymap.set("n", "<leader>tp", function()
+                builtin.planets({ show_pluto = true })
+            end, { desc = "Telescope: [T]oggle [P]lanets" })
+        end,
+    },
 }
