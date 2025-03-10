b805fa889a8df81c708a180e6e596f20bc5a9574
idiasdas <igordias.ti@gmail.com>
2025-03-10 19:01:54 +0100

    Fix lockscreen

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-convert $HOME/Pictures/wallpaper.jpg -resize $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/') RGB:- | i3lock -e --noinput-text="" --verif-text="" --wrong-text="" --ind-pos="x + 40:h - 40" --radius 30 --raw $(xdpyinfo | grep dimensions | sed -r 's/^[^0-9]*([0-9]+x[0-9]+).*$/\1/'):rgb --image /dev/stdin --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
-    --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
-    --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
-    --ringver-color=ffffffff --ringwrong-color=ffffffff
+if [[ -f $HOME/Pictures/wallpaper.png ]]; then
+    i3lock -e --noinput-text="" --verif-text="" --wrong-text="" --ind-pos="x + 40:h - 40" --radius 30 \
+        --image $HOME/Pictures/wallpaper.png --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+        --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+        --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
+        --ringver-color=ffffffff --ringwrong-color=ffffffff \
+        -F
+else
+    i3lock -e --noinput-text="" --verif-text="" --wrong-text="" --ind-pos="x + 40:h - 40" --radius 30 \
+        --color 000000 \
+        --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+        --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
+        --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
+        --ringver-color=ffffffff --ringwrong-color=ffffffff
+fi
 
