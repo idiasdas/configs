49f20f16724175c33436a788fc43eaf9e55e89c8
idiasdas <igordias.ti@gmail.com>
2025-03-11 19:45:02 +0100

    Show tray only on primary monitor

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -7,11 +7,12 @@ polybar-msg cmd quit
 # killall -q polybar
 #
 
-bar=mybar
 if type "xrandr"; then
   for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    bar=$(xrandr --query | grep $m | [[ -z $(grep " primary") ]] && echo "mybar_notray" || echo "mybar")
     MONITOR=$m polybar $bar 2>&1 | tee -a /tmp/polybar.log & disown
   done
 else
-   polybar $bar 2>&1 | tee -a /tmp/polybar.log & disown
+    tray_position="right"
+    polybar mybar 2>&1 | tee -a /tmp/polybar.log & disown
 fi
