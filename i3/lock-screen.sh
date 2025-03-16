ab3c4c8a1a971e54573096163837e8d4810811c5
idiasdas <igordias.ti@gmail.com>
2025-03-16 23:43:20 +0100

    Use different lockscreen

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 
-if [[ -f $HOME/Pictures/wallpaper.png ]]; then
+picture=$HOME/Pictures/lockscreen.png 
+if [[ -f $picture ]]; then
     i3lock -e --noinput-text="" --verif-text="" --wrong-text="" --ind-pos="x + 40:h - 40" --radius 30 \
-        --image $HOME/Pictures/wallpaper.png --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
+        --image $picture --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
         --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
         --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
         --ringver-color=ffffffff --ringwrong-color=ffffffff \
