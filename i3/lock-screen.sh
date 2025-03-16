0a9b8dde9462ed7dea9cf598de96eb60f6f9747f
idiasdas <igordias.ti@gmail.com>
2025-03-17 00:01:29 +0100

    Remove suspend after lock

--- i3/lock-screen.sh
+++ i3/lock-screen.sh
@@ -7,13 +7,13 @@ if [[ -f $picture ]]; then
         --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
         --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
         --ringver-color=ffffffff --ringwrong-color=ffffffff \
-        -F && systemctl suspend
+        -F
 else
     i3lock -e --noinput-text="" --verif-text="" --wrong-text="" --ind-pos="x + 40:h - 40" --radius 30 \
         --color 000000 \
         --inside-color=373445ff --ring-color=ffffffff --line-uses-inside \
         --keyhl-color=d23c3dff --bshl-color=d23c3dff --separator-color=00000000 \
         --insidever-color=fecf4dff --insidewrong-color=d23c3dff \
-        --ringver-color=ffffffff --ringwrong-color=ffffffff && systemctl suspend
+        --ringver-color=ffffffff --ringwrong-color=ffffffff
 fi
 
