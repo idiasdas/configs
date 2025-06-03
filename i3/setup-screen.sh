8c24695c4da016e3eebdabb14f5aba3a97d0b840
idiasdas <igordias.ti@gmail.com>
2025-06-03 22:29:03 +0200

    Update screen setup script

--- i3/setup-screen.sh
+++ i3/setup-screen.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-if [[ -n $(xrandr | grep " connected" | grep -E "HDMI-1-0") ]]; then
-    xrandr --output eDP-1 --off
-    xrandr --output HDMI-1-0 --primary
-elif [[ -n $(xrandr | grep " connected" | grep -E "DP-1-0") ]]; then
-    xrandr --output eDP-1 --off
-    xrandr --output DP-1-0 --primary
-fi
+xrandr --output eDP-1 --primary --auto
+monitors="HDMI-1 HDMI-1-0 DP-1 DP-1-0"
+for monitor in $monitors; do
+    if [[ -n $(xrandr | grep " connected" | grep -E "^$monitor ") ]]; then
+        xrandr --output eDP-1 --noprimary
+        xrandr --output eDP-1 --off
+        xrandr --output $monitor --primary --auto
+        break
+    fi
+done
