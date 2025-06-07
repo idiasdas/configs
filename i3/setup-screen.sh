42614c3b19a5fa0ac621ee4cba5fe9bcc3e65e68
idiasdas <igordias.ti@gmail.com>
2025-06-07 13:29:14 +0200

    Setup autorandr on i3 start up

--- i3/setup-screen.sh
+++ i3/setup-screen.sh
@@ -3,6 +3,7 @@
 xrandr --output eDP-1 --primary --auto
 monitors="HDMI-1 HDMI-1-0 DP-1 DP-1-0"
 for monitor in $monitors; do
+    # echo $monitor
     if [[ -n $(xrandr | grep " connected" | grep -E "^$monitor ") ]]; then
         xrandr --output eDP-1 --noprimary
         xrandr --output eDP-1 --off
