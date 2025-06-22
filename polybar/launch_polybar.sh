863e4e643506eedf06ab3c8e495423962f13db92
idiasdas <igordias.ti@gmail.com>
2025-06-22 23:36:09 +0200

    Update polybar

--- polybar/launch_polybar.sh
+++ polybar/launch_polybar.sh
@@ -10,13 +10,15 @@ polybar-msg cmd quit
 if type "xrandr"; then
     for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
         bar="mybar_notray"
-        if [[ -z $(xrandr --query | grep -e "$m" | grep " primary") ]]; then
+        if [[ -n $(xrandr --query | grep -e "$m" | grep " primary") ]]; then
             bar="mybar"
         fi
         # bar=$(xrandr --query | grep $m | [[ -z $(grep " primary") ]] && echo "mybar" || echo "mybar_notray")
+        echo "Launching polybar on $m -- $bar"
         MONITOR="$m" polybar $bar --config=$HOME/configs/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
     done
 else
     tray_position="right"
+    echo "Launching default polybar"
     polybar mybar --config=$HOME/configs/polybar/config.ini 2>&1 | tee -a /tmp/polybar.log & disown
 fi
