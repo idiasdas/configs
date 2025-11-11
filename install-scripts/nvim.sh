ca02fded9b0361a45778d5fa613c217cf96c7758
idiasdas <igordias.ti@gmail.com>
2025-11-11 13:38:56 +0100

    Add check for nvim repo before clone and remove comment

--- install-scripts/nvim.sh
+++ install-scripts/nvim.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-git clone -b v0.11.2 https://github.com/neovim/neovim $HOME/personal/neovim
-sudo apt install ninja-build gettext cmake unzip curl build-essential xclip
+if [[ ! -d $HOME/personal/neovim/ ]]; then
+    git clone -b v0.11.2 https://github.com/neovim/neovim $HOME/personal/neovim
+fi
 
 cd $HOME/personal/neovim
+sudo apt install ninja-build gettext cmake unzip curl build-essential xclip
 make CMAKE_BUILD_TYPE=RelWithDebInfo CMAKE_INSTALL_PREFIX=$HOME/personal/neovim
 make install
